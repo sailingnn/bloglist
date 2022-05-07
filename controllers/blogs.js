27ce4552f3e8612279f67a769608a402685964de
sailingnn <457886446@qq.com>
2022-05-07 13:33:15 +0800

    part4.10: Blog list tests, step3

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,40 @@
 const blogsRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
+// route handler using promises
+// blogsRouter.get('/', (request, response) => {
+//     Blog
+//       .find({})
+//       .then(blogs => {
+//         response.json(blogs)
+//       })
+//   })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+// route handler using async/await
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})  
 
-    blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
-        })
+// route handler using promises
+// blogsRouter.post('/', (request, response) => {
+//     const blog = new Blog(request.body)
+
+//     // console.log('request.body', request.body)
+//     // console.log('blog:', blog)
+
+//     blog
+//         .save()
+//         .then(result => {
+//         response.status(201).json(result)
+//         })
+// })
+
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  console.log('cleared')
+  initialBlogs.forEach(async (blog) => {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+    console.log('saved')
+  })
+  console.log('done')
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -21,6 +47,28 @@ test('the unique identifier property of the blog posts is named id', async () =>
   const response = await api.get('/api/blogs')
   response.body.map(n => expect(n.id).toBeDefined)
 })
+
+test.only('a valid new blog post can be added', async () => {
+  const newBlog = {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  }
+  await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  expect(titles).toContain(
+    'First class tests'
+  )
+}, 100000)
 
 afterAll(() => {
   mongoose.connection.close()
