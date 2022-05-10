16a96ce1dae66a0b263ece38833d216918ffc3df
sailingnn <457886446@qq.com>
2022-05-10 14:23:59 +0800

    part4.14 Blog list expansions, step2

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -50,4 +50,32 @@ blogsRouter.post('/', async (request, response) => {
   // }
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  // console.log('body:', body)
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true })
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
